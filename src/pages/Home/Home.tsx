e4eb01fc8e2c93d124142686c8b66b3702f6c1a7
Facundo Masgrau <facundomasgrau@gmail.com>
2021-08-29 16:45:06 +0000

    Initial commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,14 @@
-import React, { Fragment } from "react";
-import Counter from "../../features/counter/Counter";
+import React from "react";
 import "./Home.scss";
 
 const Home: React.FC = () => (
   <>
     <div>
-      <h1>Hello Bravo!</h1>
+      <h1>Hello Facundo!</h1>
       <p>
         Here is an example of state management with redux, feel free to copy or
         replace the code with yours :)
       </p>
-      <Counter />
     </div>
   </>
 );
